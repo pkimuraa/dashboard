cc8bf7cff065409a200ea653e443ca1ff0c3eb0f
pkimuraa <paulo.kimura94@gmail.com>
2023-03-29 21:33:34 -0300

    Added tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,33 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+
+    colors: {
+      'primary': '#18122B',
+      'secondary': '#393053',
+      'shade': '#443C68',
+      'highlight': '#635985',
+ 
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
   },
   plugins: [],
 }

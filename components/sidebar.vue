260592f939650512d33f88c9b4ffe20d12229a02
pkimuraa <paulo.kimura94@gmail.com>
2023-03-30 22:41:12 -0300

    Added menu

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="fixed h-screen w-80 border bg-highlight"></div>
+  <div class="fixed h-screen w-80 border bg-highlight m-1 border-t-shade p-4">
+    <!-- Select Team-->
+    <span class="w-full flex">
+      <span class="w-12 h-12 border mr-4"> </span>
+      <span class="w-full"> NOME TEAM </span>
+    </span>
+    <!-- MENU-->
+    <span class="w-full flex flex-wrap mt-4 border-t-2 border-opacity-30">
+      <ul>
+        <li>Home</li>
+        <li>My Team</li>
+        <li>Leaderboard</li>
+      </ul>
+    </span>
+  </div>
 </template>
 
 <script>
